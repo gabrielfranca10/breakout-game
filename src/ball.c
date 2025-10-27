82dc1611b895dd8f3f1b12119f3e90ce5601f98d
Fernadosoares10 <nandosoares719@gmail.com>
2025-10-27 12:01:40 -0300

    ajustando  bug da bola presa nas paredes

--- src/ball.c
+++ src/ball.c
@@ -16,11 +16,23 @@ void UpdateBall(Ball *ball, int screenWidth, int screenHeight)
     ball->position.x += ball->speed.x * GetFrameTime();
     ball->position.y += ball->speed.y * GetFrameTime();
 
-    if ((ball->position.x - ball->radius) <= 0 || (ball->position.x + ball->radius) >= screenWidth)
+    if ((ball->position.x - ball->radius) <= 0)
+    {
         ball->speed.x *= -1;
+        ball->position.x = ball->radius;
+    }
+    
+    if ((ball->position.x + ball->radius) >= screenWidth)
+    {
+        ball->speed.x *= -1;
+        ball->position.x = screenWidth - ball->radius;
+    }
 
     if ((ball->position.y - ball->radius) <= 0)
+    {
         ball->speed.y *= -1;
+        ball->position.y = ball->radius;
+    }
 }
 
 void DrawBall(Ball ball)
